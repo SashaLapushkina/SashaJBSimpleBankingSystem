5ca6dfed524ee163a5ed2f5cab56cd7fadf13420
Sasha <sashalapushkina@mail.ru>
2021-08-26 10:00:29 +0300

    Массив заменен на БД

--- main.py
+++ main.py
@@ -3,50 +3,43 @@ import sqlite3
 
 connection = sqlite3.connect('card.s3db')
 cursor = connection.cursor()
-cursor.execute("CREATE TABLE IF NOT EXIST cards ()")
+cursor.execute("CREATE TABLE IF NOT EXISTS cards (id INT, pin INT, balance INT)")
+connection.commit()
 
-class Card:
-    def __init__(self, id, pin):
-        self.id = id
-        self.pin = pin
-        self.balance = 0
-
-    def show_balance(self):
-        print("Balance: " + str(self.balance))
-
-    def __eq__(self, other):
-        return self.id == other.id and self.pin == other.pin
+def print_data():
+    cursor.execute("SELECT * FROM cards")
+    print(cursor.fetchall())
 
 def exit_():
     print("Bye!")
     exit(0)
 
 def check_luhn(id):
-    ids = list(id)
+    ids = list(str(id))
     sum = 0
-    map(int, ids)
     for i in range(len(ids)):
         if i % 2 == 0:
-            ids[i] *= 2
-        if ids[i] > 9:
-            sum += ids[i] - 9
+            ids[i] = int(ids[i]) * 2
+        if int(ids[i]) > 9:
+            sum += int(ids[i]) - 9
         else:
-            sum += ids[i]
+            sum += int(ids[i])
     return (10 - sum % 10) % 10
 
 def create():
-    global cards
-    id = (4000000000000000 + random.randint(100000000, 999999999)) * 10
+    id = (400000000000000 + random.randint(100000000, 999999999)) * 10
     id += check_luhn(id)
     pin = random.randint(0, 9999)
-    cards.append(Card(id, pin))
+    cursor.execute("INSERT INTO cards values ({}, {}, 0)".format(id, pin))
+    connection.commit()
+    print_data()
     print("Your card has been created")
     print("Your card id:")
     print(id)
     print("Your card PIN:")
     print("%04i" % pin)
 
-def account(card):
+def account(id, pin):
     print("1. Balance")
     print("2. Log out")
     print("0. Exit")
@@ -54,8 +47,9 @@ def account(card):
     command = input()
 
     if command == '1':
-        card.show_balance()
-        account(card)
+        cursor.execute("SELECT balance FROM cards WHERE id = {} and pin = {}".format(id, pin))
+        print(cursor.fetchone()[0])
+        account(id, pin)
     elif command == '2':
         print("You have successfully logged out!")
         menu()
@@ -64,19 +58,15 @@ def account(card):
 
 def log_in():
     print("Enter your card id:")
-    id = input()
+    id = int(input())
     print("Enter your PIN:")
     pin = int(input())
-    new_card = Card(id, pin)
-    dont_exist = True
-    for card in cards:
-        if card == new_card:
-            print("You have successfully logged in!")
-            account(card)
-            dont_exist = False
-    if dont_exist:
+    cursor.execute("SELECT * FROM cards WHERE id = {} and pin = {}".format(id, pin))
+    if cursor.fetchone() == None:
         print("Wrong card id or PIN!")
-
+    else:
+        print("You have successfully logged in!")
+        account(id, pin)
 
 def menu():
     print("1. Create an account")
@@ -94,6 +84,4 @@ def menu():
     elif command == '0':
         exit_()
 
-cards = []
-#menu()
-print(check_luhn("400000261634864"))
+menu()
